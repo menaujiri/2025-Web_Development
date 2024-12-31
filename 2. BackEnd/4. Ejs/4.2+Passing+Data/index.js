f98a5317fc90b44dd9ca60ff164c89151d8f0ba6
menaujiri <ujirimena@gmail.com>
2024-12-31 06:10:15 +0100

    ejs tag

--- 2. BackEnd/4. Ejs/4.2+Passing+Data/index.js
+++ 2. BackEnd/4. Ejs/4.2+Passing+Data/index.js
@@ -10,7 +10,10 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const numLetter = req.body["fName"].length + req.body["lName"].length;
+  res.render("index.ejs", { numberOfLetter: numLetter });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
